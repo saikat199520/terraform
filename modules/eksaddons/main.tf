bcd9e5d1ab0dced77fcf64b26b28add455f23a69
saikat <sktsadhukha@gmail.com>
2026-08-21 13:30:36 +0530

    comments_added

--- modules/eksaddons/main.tf
+++ modules/eksaddons/main.tf
@@ -2,7 +2,8 @@ resource "aws_eks_addon" "ebs_csi" {
   cluster_name = var.eks_cluster_name
   addon_name = "aws-ebs-csi-driver"
   service_account_role_arn = var.aws_eks_extra_storage_roles
-  configuration_values = jsonencode({
+  
+  configuration_values = jsonencode({  ######have reduced the replica set to 1 for budget constraint
     controller = {
       replicaCount = 1
     }
@@ -13,21 +14,14 @@ resource "aws_eks_addon" "efs_csi" {
   cluster_name = var.eks_cluster_name
   addon_name = "aws-efs-csi-driver"
   service_account_role_arn = var.aws_eks_extra_storage_roles
-  configuration_values = jsonencode({
+  
+  configuration_values = jsonencode({  ######have reduced the replica set to 1 for budget constraint
     controller = {
       replicaCount = 1
     }
   })
 
 }
-
-#resource "aws_eks_addon" "vpc_cni" {
-#  cluster_name = var.eks_cluster_name
-#  addon_name = "vpc-cni"
-#  service_account_role_arn = var.aws_eks_vpc_cni_role
-#  resolve_conflicts_on_update = "OVERWRITE"
-#  resolve_conflicts_on_create = "OVERWRITE"
-#}
 
 resource "aws_eks_addon" "secrets_store" {
   cluster_name = var.eks_cluster_name
@@ -37,15 +31,25 @@ resource "aws_eks_addon" "secrets_store" {
 resource "aws_eks_addon" "metrics_server" {
   cluster_name = var.eks_cluster_name
   addon_name = "metrics-server"
-  configuration_values = jsonencode({
+  
+  configuration_values = jsonencode({  ######have reduced the replica set to 1 for budget constraint
     replicas = 1
   })
 
 }
-#resource "aws_eks_addon" "kube_state_metrics" {
+#resource "aws_eks_addon" "kube_state_metrics" {  #### if using prometheus you can turn this on and turn off metrics server
 #  cluster_name = var.eks_cluster_name
 #  addon_name = "kube-state-metrics"
-#  configuration_values = jsonencode({
+#
+#  configuration_values = jsonencode({  ######have reduced the replica set to 1 for budget constraint
 #     replicas = 1
 #  })
+#}
+############           marked for removal on a later date
+#resource "aws_eks_addon" "vpc_cni" {
+#  cluster_name = var.eks_cluster_name
+#  addon_name = "vpc-cni"
+#  service_account_role_arn = var.aws_eks_vpc_cni_role
+#  resolve_conflicts_on_update = "OVERWRITE"
+#  resolve_conflicts_on_create = "OVERWRITE"
 #}
