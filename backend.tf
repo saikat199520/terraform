0563d8a3adcbb5372c5b2a58eccea7b49aa3ea58
saikat <sktsadhukha@gmail.com>
2026-08-22 12:55:30 +0530

    backend.tf_transfered_to_gitignore_dummy_example_backend_added

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ terraform {
         region         = "ap-south-2"                          # Your AWS Region
         use_lockfile   = true                  # Matches DynamoDB table created above
         encrypt        = true
-        kms_key_id   = "arn:aws:kms:ap-south-2:207791567608:alias/aws/s3"
+        kms_key_id   = "arn:aws:kms:ap-south-2:207791567608:alias/aws/s3" #dummy account
         profile      = "hi"
     }
 }
