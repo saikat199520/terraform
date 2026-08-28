495aa6834b048dd3a69cdbdc57b1311e59604d36
saikat <sktsadhukha@gmail.com>
2026-08-28 15:53:47 +0530

    precommit_helm_version_uodated_to_3

--- providers.tf
+++ providers.tf
@@ -1,34 +1,34 @@
-terraform{
-    required_version = ">=1.12.0"
+terraform {
+  required_version = ">=1.12.0"
 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>5.0"
-        }
-        helm = {
-            source = "hashicorp/helm"
-            version = "~> 2.12"
-        }
-        kubernetes = {
-            source = "hashicorp/kubernetes"
-            version = "~>2.20"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~>2.20"
+    }
+  }
 }
-provider "aws"{
-   region = var.aws_region
-   profile = var.profile
+provider "aws" {
+  region  = var.aws_region
+  profile = var.profile
 }
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name,"--profile", var.profile, "--region", var.aws_region]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--profile", var.profile, "--region", var.aws_region]
     }
   }
 }
