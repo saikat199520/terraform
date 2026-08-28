ec9a01ae202a2b42f3a767064293c321aad94c9e
saikat <sktsadhukha@gmail.com>
2026-08-28 16:00:03 +0530

    eksmodules_updated_according_to_helm_version

--- modules/eksaddons/main.tf
+++ modules/eksaddons/main.tf
@@ -37,139 +37,128 @@ resource "aws_eks_addon" "metrics_server" {
   })
 }
 
-resource "helm_release" "cert_manager" {
-  name = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart = "cert-manager"
-  namespace = "cert-manager"
-  version = "v1.21.1"
+esource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  version          = "v1.21.1"
   create_namespace = true
 
-  set {
-    name = "installCRDs"
-    value = "true"
-  }
-  
-  set {
-    name  = "replicaCount"
-    value = "1"
-  }
-
-  set {
-    name  = "webhook.replicaCount"
-    value = "1"
-  }
-
-  set {
-    name  = "cainjector.replicaCount"
-    value = "1"
-  }
-
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    },
+    {
+      name  = "replicaCount"
+      value = "1"
+    },
+    {
+      name  = "webhook.replicaCount"
+      value = "1"
+    },
+    {
+      name  = "cainjector.replicaCount"
+      value = "1"
+    },
+  ]
 }
 
 resource "helm_release" "aws_load_balancer_controller" {
-  name = "aws-load-balancer-controller"
+  name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
-  chart = "aws-load-balancer-controller"
-  namespace = "kube-system"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
 
-  set {
-    name = "clusterName"
-    value = var.eks_cluster_name
-  }
-
-  set {
-    name  = "replicaCount" # ######have reduced the replica set to 1 for budget constraint
+  set = [
+    {
+      name  = "clusterName"
+      value = var.eks_cluster_name
+    },
+    {
+      name  = "replicaCount" # ######have reduced the replica set to 1 for budget constraint
       value = "1"
-  }
-
-  #set {
-  #  name = "serviceAccount.create"
-  #  value = "false"
-    
-  #}
-
-  set {
-    name = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+    },
+    # {
+    #    name = "serviceAccount.create"
+    #    value = "false"
+    #}
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    }
+  ]
   depends_on = [helm_release.cert_manager]
 }
 
 resource "helm_release" "ingress_nginx" {
-  name = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart = "ingress-nginx"
-  namespace = "ingress-nginx"
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
   create_namespace = true
-  version = "4.15.1"
+  version          = "4.15.1"
 
-  set {
-    name = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-    value = "external"
-  }
-
-  set {
-    name = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-nlb-target-type"
-    value = "ip"
-  }
-
-  set {
-    name = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
-    value = "internet-facing"
-  }
-
-  set { # ataching ssl certificated
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
-    value = var.ssl_arn # Reference your ACM certificate ARN
-  }
-
-  set { # 443 from the Load Balancer
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
-    value = "https"
-  }
-
-  set { # terminating ssl at nlb
-    name  = "controller.service.targetPorts.https"
-    value = "http"
-  }
-
-
+  set = [
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+      value = "external"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-nlb-target-type"
+      value = "ip"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+      value = "internet-facing"
+    },
+    { # ataching ssl certificated
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
+      value = var.ssl_arn # Reference your ACM certificate ARN
+    },
+    { # 443 from the Load Balancer
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
+      value = "https"
+    },
+    { # terminating ssl at nlb
+      name  = "controller.service.targetPorts.https"
+      value = "http"
+    }
+  ]
   depends_on = [helm_release.aws_load_balancer_controller]
 }
 
 resource "helm_release" "argocd" {
-  name = "argocd"
-  repository= "https://argoproj.github.io/argo-helm"
-  chart = "argo-cd"
-  namespace = "argo-cd"
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argo-cd"
   create_namespace = true
-  version = "8.2.6"
- # depends_on = [helm_release.ingress_nginx]
-  set {
-    name  = "server.ingress.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "server.ingress.ingressClassName"
-    value = "nginx"
-  }
-
-  set {
-    name  = "global.domain"
-    value = "argocd.${var.domain}"
-  }
-
-  set {
-    name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/backend-protocol"
-    value = "HTTPS"
-  }
-
-  set {
-    name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/force-ssl-redirect"
-    value = "false"
-  }
+  version          = "8.2.6"
+  # depends_on = [helm_release.ingress_nginx]
+  set = [
+    {
+      name  = "server.ingress.enabled"
+      value = "true"
+    },
+    {
+      name  = "server.ingress.ingressClassName"
+      value = "nginx"
+    },
+    {
+      name  = "global.domain"
+      value = "argocd.${var.domain}"
+    },
+    {
+      name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/backend-protocol"
+      value = "HTTPS"
+    },
+    {
+      name  = "server.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/force-ssl-redirect"
+      value = "false"
+    }
+  ]
 }
 
 
